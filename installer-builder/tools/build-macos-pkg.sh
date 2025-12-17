daabb6f21c2d51098ce05f1d69db551e8b754c24
ayush-panta <ayushkp@amazon.com>
2025-12-16 17:35:16 -0800

    Add plsit handling on install for mac
    Signed-off-by: ayush-panta <ayushkp@amazon.com>

--- installer-builder/tools/build-macos-pkg.sh
+++ installer-builder/tools/build-macos-pkg.sh
@@ -40,6 +40,13 @@ buildPkgInstaller() {
     cp ./installer-builder/darwin/Resources/uninstall.sh $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch
     sed -i '' -e 's/__VERSION__/'"${VERSION}"'/g' $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/uninstall.sh
     
+    #copy credential helper tools and templates, need to be accessible by postinstall and for re-init on error
+    mkdir -p $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/tools
+    cp ./installer-builder/templates/com.runfinch.cred-bridge.plist.template $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/tools/
+    cp ./installer-builder/tools/native-creds-agent-start.sh $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/tools/
+    cp ./installer-builder/tools/native-creds-agent-stop.sh $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/tools/
+    chmod +x $INSTALLER_FULL_PATH/darwinpkg/Applications/Finch/tools/native-creds-agent-*.sh
+    
     #construct pkg directory
     mkdir -p $INSTALLER_FULL_PATH/package
     mkdir -p $INSTALLER_FULL_PATH/signed
